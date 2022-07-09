eeffe8fc16f4bff45a1d391edffd92c76ca8c98d
Dave Kimmel <davidjkimmel@gmail.com>
2022-07-09 07:55:44 -0600

    Improved error handling

--- DSShim.py
+++ DSShim.py
@@ -19,35 +19,38 @@ def process_image():
     f = request.files['image']
     image = {"Bytes": f.stream.read()}
 
+    dsresp = {'predictions': [], 'success': True}
+
     # Call Rekognition
     logger.info("Calling Rekognition")
-    resp = rekognition_client.detect_labels(Image=image)
-    logger.info("Done")
+    try:
+        resp = rekognition_client.detect_labels(Image=image)
+        requestid = resp['ResponseMetadata']['RequestId']
+        statuscode = resp['ResponseMetadata']['HTTPStatusCode']
+        logger.info(f"Rekognition RequestID: {requestid}, Status: {statuscode}")
 
-    # Now convert the AWS response into something resembling what DeepStack will return
-
-    # Open the image so that we can determine its width and height
-    # Rekognition returns label locations in a range of 0-1, but DeepStack returns them in pixels
-    img = Image.open(io.BytesIO(image['Bytes']))
-    
-    dsresp = {'predictions': [], 'success': True}
-    for l in resp['Labels']:
-        for i in l['Instances']:
-            bb = i['BoundingBox']
-            top = bb['Top'] * img.height
-            bottom = bb['Height'] * img.height + top
-            left = bb['Left'] * img.width
-            right = bb['Width'] * img.width + left
-            ds = {'x_min': int(left), 
-                'x_max': int(right), 
-                'y_min': int(top),
-                'y_max': int(bottom), 
-                'confidence': i['Confidence'] / 100,
-                'label': l['Name']}
-            dsresp['predictions'].append(ds)
+        # Now convert the AWS response into something resembling what DeepStack will return
+        with Image.open(io.BytesIO(image['Bytes'])) as img:
+            for l in resp['Labels']:
+                for i in l['Instances']:
+                    # Rekognition returns label locations in a range of 0-1, but DeepStack returns them in pixels
+                    bb = i['BoundingBox']
+                    top = bb['Top'] * img.height
+                    bottom = bb['Height'] * img.height + top
+                    left = bb['Left'] * img.width
+                    right = bb['Width'] * img.width + left
+                    ds = {'x_min': int(left), 
+                        'x_max': int(right), 
+                        'y_min': int(top),
+                        'y_max': int(bottom), 
+                        'confidence': i['Confidence'] / 100,
+                        'label': l['Name']}
+                    dsresp['predictions'].append(ds)
+    except ClientError:
+        logger.error("Error calling Rekognition")
+        dsresp['success'] = False
 
     return jsonify(dsresp)
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
